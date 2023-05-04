fb2a291b3eb83d2c71e7f51e46c2e5e67de27499
Abebayehu Tefera <abebayehutefera@gmail.com>
2023-05-04 06:33:32 -0700

    fix

--- 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
+++ 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
@@ -1,6 +1,4 @@
 #!/usr/bin/env ruby
-# This script accepts one argument and passes it to a regular expression,
-# matching method
-# This script should output: [SENDER],[RECEIVER],[FLAGS]
+# Only match capital letters
 
-puts ARGV[0].scan(/(?<=from:|to:|flags:).+?(?=\])/).join(',')
+puts ARGV[0].scan(/[A-Z]/).join
