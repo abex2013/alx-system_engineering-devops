119fccb1e171a14779327949a93286080962bb80
abex2013 <abexazol@gmail.com>
2023-08-14 11:47:07 +0000

    fix

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,45 +1,54 @@
 #!/usr/bin/python3
-"""
-Queries the Reddit API, parses the title of all hot articles, and prints a
-sorted count of given keywords.
-"""
+""" raddit api"""
+
+import json
 import requests
 
 
-def count_words(subreddit, word_list, hot_list=[], viewed_count=0, after=''):
-    """
-    Queries the Reddit API, parses the title of all hot articles, and prints a
-    sorted count of given keywords.
-    """
-    base = 'https://www.reddit.com/'
-    endpoint = 'r/{}/hot.json'.format(subreddit)
-    query_string = '?show="all"&limit=100&after={}&count={}'.format(
-        after, viewed_count)
-    url = base + endpoint + query_string
-    headers = {'User-Agent': 'Python/1.0(Holberton School 0x16 task 3)'}
-    response = requests.get(url, headers=headers)
-    if not response.ok:
-            return
+def count_words(subreddit, word_list, after="", count=[]):
+    """count all words"""
 
-    data = response.json()['data']
-    for post in data['children']:
-        hot_list.append(post['data']['title'])
-    after = data['after']
-    dist = data['dist']
-    if (after):
-        count_words(subreddit, [], hot_list, viewed_count + dist, after)
+    if after == "":
+        count = [0] * len(word_list)
 
-    if viewed_count == 0:
-        result = {}
-        word_list = [word.lower() for word in word_list]
-        hot_words = ' '.join(hot_list).lower().split(' ')
-        for hot_word in hot_words:
-            for search_word in word_list:
-                if hot_word == search_word:
-                    result.setdefault(search_word, 0)
-                    result[search_word] += 1
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    request = requests.get(url,
+                           params={'after': after},
+                           allow_redirects=False,
+                           headers={'user-agent': 'bhalut'})
 
-        for word, count in sorted(
-            sorted(result.items()), key=lambda x: x[1], reverse=True
-        ):
-            print("{}: {}".format(word, count))
+    if request.status_code == 200:
+        data = request.json()
+
+        for topic in (data['data']['children']):
+            for word in topic['data']['title'].split():
+                for i in range(len(word_list)):
+                    if word_list[i].lower() == word.lower():
+                        count[i] += 1
+
+        after = data['data']['after']
+        if after is None:
+            save = []
+            for i in range(len(word_list)):
+                for j in range(i + 1, len(word_list)):
+                    if word_list[i].lower() == word_list[j].lower():
+                        save.append(j)
+                        count[i] += count[j]
+
+            for i in range(len(word_list)):
+                for j in range(i, len(word_list)):
+                    if (count[j] > count[i] or
+                            (word_list[i] > word_list[j] and
+                             count[j] == count[i])):
+                        aux = count[i]
+                        count[i] = count[j]
+                        count[j] = aux
+                        aux = word_list[i]
+                        word_list[i] = word_list[j]
+                        word_list[j] = aux
+
+            for i in range(len(word_list)):
+                if (count[i] > 0) and i not in save:
+                    print("{}: {}".format(word_list[i].lower(), count[i]))
+        else:
+            count_words(subreddit, word_list, after, count)
